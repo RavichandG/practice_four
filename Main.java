83683f9d1fc7faa1c282d125418fc60682c2e9ed
ravi chand <135828105+RavichandG@users.noreply.github.com>
2025-03-09 22:42:43 +0530

    add addition feature

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
 public class Main {
-
+    public static void add(int a, int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        Main.add(2,3);
+    }
 }
